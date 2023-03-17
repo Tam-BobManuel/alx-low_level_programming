491c4a6f638515d74800ab475bd2936a3f3183e1
Tam-BobManuel <bglorytb@gmail.com>
2023-03-17 09:21:16 -0700

    correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
+
 /**
  * main - Prints numbers between 00 to 89.
- *Seperates them with a comma and space
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j
+	int i, e;
 
-	for (i = 0; i < 10; i++)
-	for (j = i + 1; j < 10; j++)
-		putchar(i + '0');
-		putchar(j + '0');
-	if (i == 8 && j == 9)
-		putchar('\n');
-	else
-		putchar(',');
-		putchar(' ');
+	i = 48;
+	e = 48;
 
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
 	return (0);
 }
+
 
