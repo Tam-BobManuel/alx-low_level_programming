961c885906f3daf8dc15c69d0f018c058ed98321
Tam-BobManuel <bglorytb@gmail.com>
2023-03-30 07:26:56 -0700

    0x06 done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x06-pointers_arrays_strings, done o the second day of the task, more study needs to be done
